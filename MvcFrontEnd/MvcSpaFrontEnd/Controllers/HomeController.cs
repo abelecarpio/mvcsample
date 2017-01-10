26bdb6447b3822b3dc071556af2a699f6e16f51f
abelecarpio@live.com.ph <abelecarpio@live.com.ph>
2017-01-10 11:34:04 +0800

    wew

---
+++ MvcFrontEnd/MvcSpaFrontEnd/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace MvcSpaFrontEnd.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Home Page";
+            ViewBag.Message = "Modify this template to jump-start your ASP.NET MVC application.";
+
+            return View();
+        }
+    }
+}
